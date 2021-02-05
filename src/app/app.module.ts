9f2a4b06dd13d04327f29b3b325844183c517a2f
Ramiro Bahena <ramiro.bah11@gmail.com>
2021-02-05 11:00:38 -0600

    1.- Creacion de componentes 2.- Implementacion de las rutas 3.- Implementaciones de servicio 4.- Envio de parametros por  URL

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,36 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+//Componentes
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { HeroesComponent } from './components/heroes/heroes.component';
+
+//Rutas
+import { APP_ROUTING } from './app.routes';
+
+
+//Servicios
+import { heroesService } from './servicios/heroes.service';
+import { HeroeComponent } from './components/heroe/heroe.component'
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    AboutComponent,
+    HeroesComponent,
+    HeroeComponent
   ],
   imports: [
-    BrowserModule
+      BrowserModule,
+      APP_ROUTING
   ],
-  providers: [],
+  providers: [heroesService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/components/heroes/heroes.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { heroesService, Heroe } from '../../servicios/heroes.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent implements OnInit {
+
+    heroes: Heroe[] = [];
+    constructor(private _heroesService: heroesService,
+                private _route:Router) {
+    }
+
+  ngOnInit(): void {
+      this.heroes = this._heroesService.getHeroes();
+  }
+
+
+    verHeroe(idx:number) {
+        this._route.navigate(['/heroe', idx]);
+    }
+
+}
